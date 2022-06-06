b5c9bd29fc8058a17f2965a5057a12f090288420
partha028 <53009001+partha028@users.noreply.github.com>
2022-06-06 21:39:40 +0530

    Add files via upload

---
+++ src/test/java/com/stock/ise/IseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stock.ise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
